cf122cf0907de4d737bc68ca0dc9cd77a5071a1b
Nathan Bird <bir19004@byui.edu>
2022-11-15 09:38:27 -0700

    change from implicit to explicit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace CSE210_04
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
