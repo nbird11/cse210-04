c4560b235bcae700cfc1721de3510ed5574dc829
Nathan Bird <bir19004@byui.edu>
2022-11-16 09:17:15 -0700

    copy classes from rfk

---
+++ Game/Casting/Collectable.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+
+
+namespace Unit04.Game.Casting
+{
+    /// <summary>
+    /// <para>Something to collect like a rock or a gem.</para>
+    /// <para>The responsibility of Collectable is to keep track of objects that can be collected.
+    /// </para>
+    /// </summary>
+    public class Collectable : Actor
+    {
+        
+        public Collectable()
+        {
+           
+        }
+    }
+}
